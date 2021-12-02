06f2e9a60dc3fd73989f012486d21fb77f835ae7
Lukito24 <luka.orjonikidze.1@btu.edu.ge>
2021-12-02 15:28:17 +0400

    Initial commit

---
+++ app/src/main/java/com/example/lecture8/fragments/NotificationFragment.kt
@@ -0,0 +1,7 @@
+package com.example.lecture8.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.lecture8.R
+
+class NotificationFragment:Fragment(R.layout.fragment_notification) {
+}
---
+++ app/src/main/java/com/example/lecture8/fragments/ShavicxeniFragment.kt
@@ -0,0 +1,7 @@
+package com.example.lecture8.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.lecture8.R
+
+class ShavicxeniFragment: Fragment(R.layout.fragment_shavicxeni) {
+}
